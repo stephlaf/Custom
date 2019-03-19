1b6dbfe2ecab18b5489cc4d90901c17b68f6def8
Stephane Lafontaine <stephane@medqigong.ca>
2019-03-19 14:09:49 -0400

    Added method to make li bigger, but doesn't work

--- boutique.js
+++ boutique.js
@@ -1,7 +1,6 @@
 // Get language from dropdown
 
 const lng = document.querySelector("#language > div > button > img");
-// console.log(lng.title);
 
 // Modify price field in search results
 
@@ -10,11 +9,11 @@ const searchProductPrices = document.querySelectorAll(".price");
 const searchModels = document.querySelectorAll(".product-model");
 
 searchModels.forEach(function(model,i){
-  if (model.innerText.slice(-1) === "*") {
+  if (model.innerText.slice(-1) === "_") {
     if (lng.title === "English") {
-      searchProductPrices[i].innerText = "Price by size" ;
+      searchProductPrices[i].innerText = "Price by option" ;
     } else {
-      searchProductPrices[i].innerText = "Prix selon le format" ;
+      searchProductPrices[i].innerText = "Prix selon l'option" ;
     }
   }
 });
@@ -26,12 +25,23 @@ const model = document.querySelector("#content > div > div.col-sm-4 > ul:nth-chi
 const price = document.querySelector("#content > div > div.col-sm-4 > ul:nth-child(7) > li > h2");
 
 const replace = (function() {
-  if (model !== null && model.innerText.slice(-1) === "*") {
+  if (model !== null && model.innerText.slice(-1) === "_") {
     if (lng.title === "English") {
-      price.innerText = "Select a size"; 
+      price.innerText = "Select an option"; 
     } else {
-      price.innerText = "Sélectionnez un format";
+      price.innerText = "Sélectionnez une option";
     }
   }
 });
 replace()
+
+// Make sub-categories text bigger
+const h3Text = document.querySelector('h3.vie-editable-section');
+const ulSubCat = document.querySelector('#content ul');
+
+const makeTextBigger = (function() {
+  if (h3Text && (h3Text.textContent === 'Sous-catégorie(s)' || h3Text.textContent === 'Subcategories')) {
+    console.log('from makeBigger');
+  }
+});
+makeTextBigger();
